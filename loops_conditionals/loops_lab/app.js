c7007248dd04ade77563e02d7cc2fdd065f7ec0c
Brittany Servent <brittybear@Brittanys-MBP.domain>
2020-09-12 10:12:37 -0700

    loops Lab

---
+++ loops_conditionals/loops_lab/app.js
@@ -0,0 +1,57 @@
+/* console.log("am i here?");
+
+1.
+
+for (i = 50; i >= 20; i--) {
+    if (i % 3 === 0) {
+        console.log(i);
+    } */
+
+2.
+
+/* for (i = 100; i <=10; i++) {
+    console.log(i);
+} */
+
+3.
+
+/* const presidents = ["Donald", "Barack", "George Jr", "Bill", "George", "Ronald", "Jimmy", "Gerald", "Richard", "Lyndon", "John", "Dwight", "Harry", "Franklin", "Herbert", "Calvin", "Warren", "Woodrow", "William", "Theodore"];
+console.log(presidents.length);
+
+for (i = 1; i < presidents.length; i+=2) {
+    console.log(presidents[i]);
+} */
+
+4.
+
+/* const menu = [{name: "pizza", glutenFree: false},{name: "salad", glutenFree: true},{name: "donut", glutenFree: false},{name: "steak", glutenFree: true},{name: "chicken", glutenFree: true},{name: "cheeseburger", glutenFree: false}];
+console.log(menu.length);
+
+let gfMenu = [];
+
+for (i = 0; i< menu.length; i++) {
+    if (menu[i].glutenFree === true) {
+        gfMenu.push(menu[i])
+    }
+}
+
+console.log(menu)
+console.log(gfMenu); */
+/* 
+
+5.
+
+ const mixedArray = [1, 2, 3, 'taco', 4, 5, 6, 'burrito', 7, 8, 9, 'Whats the deal with airplane peanuts', 3.14, {barry: "Mannilow",}, 6, 55, null, "Old " + "Crow " + " Medicine Show", 3, 2, "I want the calzone from pizanos", "Death blow", "Firestorm", "48", 30];
+ 
+ 
+let stringMe = [];
+ for (i = 0; i < mixedArray.length; i ++) {
+     if (mixedArray[i] <= 55 !== true) {
+         stringMe.push(mixedArray[i])
+     }     
+}
+    
+ console.log(mixedArray);
+ console.log(stringMe); */
+ 
+ 
